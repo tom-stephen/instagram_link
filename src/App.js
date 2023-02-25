4efaa34b8083befce69242dd3cd4d5a1b983176e
tom-stephen <tom.stephen@ucalgary.ca>
2023-02-25 12:50:03 -0700

    added buttons and pic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import myImage from './img1.png';
 
 function App() {
+
+  function openInstagram() {
+    window.open("https://instagram.com/xctom?igshid=YmMyMTA2M2Y=");
+  }
+
+  function openFIS() {
+    window.open("https://www.fis-ski.com/DB/general/athlete-biography.html?sectorcode=CC&competitorid=226849");
+  }
+
+  function openFacebook() {
+    window.open("https://www.facebook.com/tom.stephen.545402?mibextid=LQQJ4d");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <img src={myImage} id="portrit" alt="My Image"/>
+      <h1 id="title">Tom Stephen</h1>
+      <p id="description">I am a web developer</p>
+      <button id="instagram-profile-link" onClick={openInstagram}>My Instagram</button>
+      <button id="fis-profile-link" onClick={openFIS}>My FIS Profile</button>
+      <button id="facebook-prifile-link" onClick={openFacebook}>Facebook</button>
     </div>
   );
 }
